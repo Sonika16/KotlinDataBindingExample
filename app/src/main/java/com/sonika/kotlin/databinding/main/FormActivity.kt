799419f2576c738a17a49b0e92b34308cef3da9c
sonika <sonika.soni2@gmail.com>
2018-02-01 22:32:50 -0800

    Initial commit

---
+++ app/src/main/java/com/sonika/kotlin/databinding/main/FormActivity.kt
@@ -0,0 +1,36 @@
+package com.sonika.kotlin.databinding.main
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.sonika.kotlin.databinding.BR
+import com.sonika.kotlin.databinding.R
+import com.sonika.kotlin.databinding.databinding.ActivityFormBinding
+
+class FormActivity : AppCompatActivity() {
+
+    private val formDataModel: FormModel = FormModel("Sonika",
+            "connect.ssonika@gmail.com","950, Harrington avenue NE",
+            1234567890);
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityFormBinding = DataBindingUtil.setContentView(this, R.layout.activity_form)
+
+        binding.setVariable(BR.form, formDataModel)
+        binding.setVariable(BR.activity, this);
+        binding.executePendingBindings()
+
+    }
+
+    fun backButtonClick(){
+       finish();
+    }
+
+    fun nextButtonClick(){
+        Toast.makeText(this, "This button is under implementation", Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/com/sonika/kotlin/databinding/main/FormModel.kt
@@ -0,0 +1,3 @@
+package com.sonika.kotlin.databinding.main
+
+data class FormModel(val name: String, val email: String, val address: String, var phoneNumber: Int)
